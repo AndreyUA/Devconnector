31922066efa4955563bd12ee89271b481b1bc46c
unknown <infernalsteel666@gmail.com>
2020-11-05 11:03:39 +0200

    delete user and profile router

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -146,4 +146,23 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+// @route       DELETE api/profile
+// @desc        Delete profile, user and posts
+// @access      Private
+
+router.delete("/", auth, async (req, res) => {
+  try {
+    //remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    //remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: "User deleted" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
