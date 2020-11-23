c884c87997ccb25fb2ac4af6f7c1fa6f9aaf6054
unknown <infernalsteel666@gmail.com>
2020-11-23 11:21:53 +0200

    create profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -24,3 +24,43 @@ export const getCurrentProfile = () => async (dispatch) => {
     });
   }
 };
+
+// Create OR update profile
+export const createProfile = (formData, history, edit = false) => async (
+  dispatch
+) => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const response = await axios.post("/api/profile", formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: response.data,
+    });
+
+    dispatch(setAlert(edit ? "Profile updated" : "Profile created", "success"));
+
+    if (!edit) {
+      history.push("/dashboard");
+    }
+  } catch (error) {
+    const errors = error.response.data.errors;
+
+    if (errors) {
+      errors.forEach((item) => dispatch(setAlert(item.msg, "danger")));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
