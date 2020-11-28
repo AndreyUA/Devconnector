e5ce81f26bc9181ca9361007b6f6177f1d1b5924
unknown <infernalsteel666@gmail.com>
2020-11-28 15:46:39 +0200

    profile github repos

---
+++ client/src/components/profile/ProfileGithub.js
@@ -0,0 +1,66 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+
+import Spinner from "../layout/Spinner";
+
+import { connect } from "react-redux";
+import { getGithubRepos } from "../../actions/profile";
+
+const ProfileGithub = ({ username, getGithubRepos, repos }) => {
+  useEffect(() => {
+    getGithubRepos(username);
+  }, [getGithubRepos, username]);
+
+  return (
+    <div className="profile-github">
+      <h2 className="text-primary my-1">Github Repos</h2>
+      {repos === null ? (
+        <Spinner />
+      ) : (
+        repos.map((item) => {
+          return (
+            <div key={item._id} className="repo bg-white p-1 my-1">
+              <div>
+                <h4>
+                  <a
+                    href={item.html_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {item.name}
+                  </a>
+                </h4>
+                <p>{item.description}</p>
+              </div>
+              <div>
+                <ul>
+                  <li className="badge badge-primaty">
+                    Stars: {item.stargazers_count}
+                  </li>
+                  <li className="badge badge-dark">
+                    Watchers: {item.watchers_count}
+                  </li>
+                  <li className="badge badge-light">
+                    Forks: {item.forks_count}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          );
+        })
+      )}
+    </div>
+  );
+};
+
+ProfileGithub.propTypes = {
+  getGithubRepos: PropTypes.func.isRequired,
+  repos: PropTypes.array.isRequired,
+  username: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  repos: state.profile.repos,
+});
+
+export default connect(mapStateToProps, { getGithubRepos })(ProfileGithub);
