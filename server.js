9c862fede120d47adbd592d4a948d167fb9cd73d
unknown <infernalsteel666@gmail.com>
2020-12-25 19:50:29 +0200

    deploy commit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const connectDB = require("./config/db");
 
+const path = require("path");
+
 const app = express();
 
 //connect to DB
@@ -9,17 +11,29 @@ connectDB();
 //init middleware
 app.use(express.json({ extended: false }));
 
+/*
 app.get("/", (req, res) => {
   res.send("API Running");
 });
-
-const PORT = process.env.PORT || 5000;
+*/
 
 // ROUTES
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// Server static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, (req, res) => {
   console.log(`server started on port ${PORT}`);
