d3421d09b7b260324a139a10bbcc63de6c7d9e54
unknown <infernalsteel666@gmail.com>
2020-12-25 16:06:12 +0200

    comments complete

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,10 +4,11 @@ import { Link } from "react-router-dom";
 
 import Spinner from "../layout/Spinner";
 import PostItem from "../posts/PostItem";
+import CommentForm from "./CommentForm";
+import CommentItem from "./CommentItem";
 
 import { connect } from "react-redux";
 import { getPost } from "../../actions/post";
-import CommentForm from "./CommentForm";
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
@@ -22,6 +23,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       </Link>
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
+      <div className="comment">
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </>
   );
 };
